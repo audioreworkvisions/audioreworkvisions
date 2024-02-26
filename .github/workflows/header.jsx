df12352b4cafeacda2160d0e888b56ae8af003de
Marco Pöschmann <posch82@swissinotechno.net>
2024-02-26 10:20:07 +0100

    Create header.jsx
    Signed-off-by: Marco Pöschmann <posch82@swissinotechno.net>

---
+++ .github/workflows/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+    return (
+        <header>
+            <h1>Willkommen bei AudioReWorkVisions</h1>
+            {/* Weitere Header-Elemente */}
+        </header>
+    );
+}
+
+export default Header;
