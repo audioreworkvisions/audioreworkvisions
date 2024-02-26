a638d907d555951747365aeda72999ed229027cd
Marco Pöschmann <posch82@swissinotechno.net>
2024-02-26 10:21:12 +0100

    Create app.jsx
    Signed-off-by: Marco Pöschmann <posch82@swissinotechno.net>

---
+++ .github/workflows/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Button from './components/Button';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Button text="Klicken Sie hier" />
+            {/* Weitere App-Inhalte */}
+        </div>
+    );
+}
+
+export default App;
